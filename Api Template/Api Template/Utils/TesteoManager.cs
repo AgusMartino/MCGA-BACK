d14dabc1a696390a76e15364d47d23c7e0c22767
ezequiel-f-a <ezequielfa99@gmail.com>
2022-10-23 17:01:44 -0300

    plantilla api

---
+++ Api Template/Api Template/Utils/TesteoManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Api_Template.Models.Template;
+using Api_Template.Entities;
+using Api_Template.Contracts;
+using System;
+using System.Linq;
+
+namespace Api_Template.Utils
+{
+    public class TesteoManager
+    {
+        #region Singleton
+        private readonly static TesteoManager _instance;
+        public static TesteoManager Current { get { return _instance; } }
+        static TesteoManager() { _instance = new TesteoManager(); }
+        private TesteoManager()
+        {
+            //Implent here the initialization of your singleton
+        }
+        #endregion
+
+        public List<Usuario> GetFromDB()
+        {
+            using (var db = new TemplateEntities())
+            {
+                return db.Usuario.ToList();
+            }
+        }
+    }
+}
