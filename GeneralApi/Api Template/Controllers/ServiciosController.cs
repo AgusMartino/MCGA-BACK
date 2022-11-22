59096009c60030d20621229e5fbe4acb79808c3e
fjescobar99 <fescobar@taglatam.com>
2022-11-22 20:36:41 -0300

    5: Se añadieron los endpoints para prender y apagar los 3 microservicios.

---
+++ GeneralApi/Api Template/Controllers/ServiciosController.cs
@@ -0,0 +1,93 @@
+﻿using GeneralApi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GeneralApi.Controllers
+{
+    public class ServiciosController : ApiController
+    {
+        [HttpPut]
+        public IHttpActionResult PutEncenderServicioMulta()
+        {
+            try
+            {
+                ServiciosManager.EncenderServicioMultas();
+                return Ok(new { msg = "Servicio de multas encendido exitosamente!" });
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+        [HttpPut]
+        public IHttpActionResult PutApagarServicioMulta()
+        {
+            try
+            {
+                ServiciosManager.ApagarServicioMultas();
+                return Ok(new { msg = "Servicio de multas apagado exitosamente!" });
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+        [HttpPut]
+        public IHttpActionResult PutEncenderServicioPagos()
+        {
+            try
+            {
+                ServiciosManager.EncenderServicioPagos();
+                return Ok(new { msg = "Servicio de pagos encendido exitosamente!" });               
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+        [HttpPut]
+        public IHttpActionResult PutApagarServicioPagos()
+        {
+            try
+            {
+                ServiciosManager.ApagarServicioPagos();
+                return Ok(new { msg = "Servicio de pagos apagado exitosamente!" });
+
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+        [HttpPut]
+        public IHttpActionResult PutEncenderServicioReconocimiento()
+        {
+            try
+            {
+                ServiciosManager.EncenderServicioReconocimiento();
+                return Ok(new { msg = "Servicio de reconocimiento encendido exitosamente!" });
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+        [HttpPut]
+        public IHttpActionResult PutApagarServicioReconocimiento()
+        {
+            try
+            {
+                ServiciosManager.ApagarServicioReconocimiento();
+                return Ok(new { msg = "Servicio de reconocimiento apagado exitosamente!" });
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
---
+++ GeneralApi/Api Template/Utils/ServiciosManager.cs
@@ -0,0 +1,56 @@
+﻿using GeneralApi.Entities.Enums;
+using GeneralApi.Models.Telepeaje;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralApi.Utils
+{
+    public class ServiciosManager
+    {
+        public static bool EncenderServicioMultas()
+        {
+            return EncenderOApagarServicio(CommonEnums.TipoServiciosEnum.Multa, true);
+        }
+        public static bool ApagarServicioMultas()
+        {
+            return EncenderOApagarServicio(CommonEnums.TipoServiciosEnum.Multa, false);
+        }
+
+        public static bool EncenderServicioPagos()
+        {
+            return EncenderOApagarServicio(CommonEnums.TipoServiciosEnum.Pago, true);
+        }
+        public static bool ApagarServicioPagos()
+        {
+            return EncenderOApagarServicio(CommonEnums.TipoServiciosEnum.Pago, false);
+        }
+
+        public static bool EncenderServicioReconocimiento()
+        {
+            return EncenderOApagarServicio(CommonEnums.TipoServiciosEnum.Reconocimiento, true);
+        }
+        public static bool ApagarServicioReconocimiento()
+        {
+            return EncenderOApagarServicio(CommonEnums.TipoServiciosEnum.Reconocimiento, false);
+        }
+
+        private static bool EncenderOApagarServicio(CommonEnums.TipoServiciosEnum servicio, bool encender_o_apagar )
+        {
+            var servicioString = EnumHelper.GetDescriptionForEnumItem( servicio );
+
+            using (var db = new TELEPEAJEEntities())
+            {
+                var s = db.Estado_Servicios
+                    .Where(x => x.Nombre_Microservicio == servicioString)
+                    .FirstOrDefault();
+                s.Estado = encender_o_apagar;
+                return db.SaveChanges() > 0;
+            }
+            return false;
+        }
+        
+    }
+}
