8e5a55ce631883a6e6df90282cce3342e92e9169
ezequiel-f-a <ezequielfa99@gmail.com>
2022-11-27 14:02:57 -0300

    ejemplo tcp

---
+++ Testeo TCP/ClienteTCP/ClienteTCP/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+// El cliente TCP va a ser el que envie info
+// En nuestro caso seria la consola que manda patentes
+
+// "Constructor" =================================
+
+while (true)
+{
+    Console.WriteLine("Ingrese una patente");
+    ExecuteClient(Console.ReadLine());
+}
+
+
+// Metodos =======================================
+
+static void ExecuteClient(string? patente)
+{
+    if (patente == null) return;
+
+    string patente_msg = patente + "<EOT>";
+
+    try
+    {
+        //config variables para armar socket y endpoint
+        var hostName = Dns.GetHostName();
+        IPHostEntry ipHost = Dns.GetHostEntry(hostName);
+        IPAddress ipAddr = ipHost.AddressList[0];
+
+        //instanciamos endpoint
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+        //instanciamos socket
+        Socket sender = new Socket(ipAddr.AddressFamily,
+                   SocketType.Stream, ProtocolType.Tcp);
+
+        //conectamos socket y endpoint
+        sender.Connect(localEndPoint);
+
+        //logeamos donde nos conectamos
+        Console.WriteLine("Socket connected to -> {0} ",
+                        sender.RemoteEndPoint?.ToString());
+
+        //creamos la info que le vamos a enviar al server
+        byte[] messageSent = Encoding.ASCII.GetBytes(patente_msg);
+
+        //enviamos la info al server
+        int byteSent = sender.Send(messageSent);
+
+        //creamos buffer de datos para recibir respuesta del server
+        byte[] messageReceived = new byte[1024];
+
+        //recibimos respuesta del server
+        int byteRecv = sender.Receive(messageReceived);
+        string msg_server = Encoding.ASCII.GetString(messageReceived,
+                                            0, byteRecv);
+
+        Console.WriteLine("Message from Server -> {0}", msg_server);
+
+        //cerramos el socket
+        sender.Shutdown(SocketShutdown.Both);
+        sender.Close();
+    }
+    catch (Exception e)
+    {
+
+        Console.WriteLine(e.ToString());
+    }
+}
---
+++ Testeo TCP/ServidorTCP/ServidorTCP/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+// El servidor TCP va a ser el que recibe info
+// En nuestro caso seria el telepeaje que recibe patentes
+
+// "Constructor" =================================
+
+ExecuteServer();
+
+// Metodos =======================================
+
+static void ExecuteServer()
+{
+    try
+    {
+        //config variables para armar socket y endpoint
+        var hostName = Dns.GetHostName();
+        IPHostEntry ipHost = Dns.GetHostEntry(hostName);
+        IPAddress ipAddr = ipHost.AddressList[0];
+
+        //instanciamos endpoint
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+        //instanciamos socket
+        Socket listener = new Socket(ipAddr.AddressFamily,
+                     SocketType.Stream, ProtocolType.Tcp);
+
+        //bindeamos socket y endpoint
+        listener.Bind(localEndPoint);
+
+        //hacemos espacio para clientes que quieran conectarse al server
+        int cantidad_max_clientes = 10;
+        listener.Listen(cantidad_max_clientes);
+
+        //entramos en un ciclo infinito de escucha iterativa
+        while (true)
+        {
+            Console.WriteLine("Waiting connection ... ");
+
+            //de haber un request de cliente, se aceptará con esto
+            Socket clientSocket = listener.Accept();
+
+            //creamos el buffer de datos
+            byte[] bytes = new Byte[1024];
+            string? data = null;
+
+            //leemos bytes hasta que ya no quede nada por leer
+            while (true)
+            {
+                int numByte = clientSocket.Receive(bytes);
+
+                string temp_data = Encoding.ASCII.GetString(bytes,
+                                            0, numByte);
+
+                data += temp_data.Replace("<EOT>","");
+
+                if (temp_data.IndexOf("<EOT>") > -1)
+                    break;
+            }
+
+            //logeamos recibido
+            Console.WriteLine("Text received -> {0} ", data);
+
+            //retornamos un msj de recibido al cliente
+            byte[] message = Encoding.ASCII.GetBytes("Mensaje recibido.");
+
+            //enviamos el msj al cliente
+            clientSocket.Send(message);
+
+            //cerramos el socket
+            //despues podemos usar el socket cerrado para una nueva conexión del cliente
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+
+            //pequeño descanso de 5ms para que no se bugee nada
+            Thread.Sleep(5);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.ToString());
+    }
+    
+}
